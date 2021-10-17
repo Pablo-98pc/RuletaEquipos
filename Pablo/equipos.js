0b1d90e029e72aebd5cd9242b23f6761b2f14ee9
Pablo <pabloperezsada98@gmail.com>
2021-10-17 19:30:23 +0200

    enter y click lista

--- Pablo/equipos.js
+++ Pablo/equipos.js
@@ -1,12 +1,19 @@
 function capture() {
-    let lista = document.getElementById("name-generated");
-    let nombre = document.getElementById("user").value;
-    let newName = document.createElement("li");
-    let contenido = document.createTextNode(nombre);
-    if (nombre == ""){
-      alert("se requiere un nombre");
-      return false
-    } 
-    newName.appendChild(contenido)
-    lista.appendChild(newName)
-} 
+  let nombre = document.getElementById("user").value;
+  let lista = document.getElementById("name-generated");
+  let newName = document.createElement("li");
+  let contenido = document.createTextNode(nombre);
+  if (nombre == ""){
+    alert("se requiere un nombre");
+    return false
+  } 
+  newName.appendChild(contenido)
+  lista.appendChild(newName)
+}
+
+function enterKeyPressed(event) {
+  let tecla = event.keyCode
+  if (tecla === 13) {
+     document.getElementById("name-btn").click();
+  } 
+}
